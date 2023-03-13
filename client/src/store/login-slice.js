c804a323f86df9332014e9cf495c8c0758552081
parthdhwajendramishra <parthdhwajendramishra@gmail.com>
2023-03-13 08:34:11 +0530

    store

---
+++ client/src/store/login-slice.js
@@ -0,0 +1,18 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const loginSlice=createSlice({
+
+    name:'login',
+    initialState:{isLogin:false},
+    reducers:{
+        loggingIn(state){
+            state.isLogin=!state.isLogin;
+        }
+    }
+});
+
+
+export const loginActions=loginSlice.actions;
+
+export default loginSlice;
