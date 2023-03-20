adf79ab5b6cc79615c5b11a06519b207bd406801
parthdhwajendramishra <parthdhwajendramishra@gmail.com>
2023-03-20 22:36:23 +0530

    auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,17 +5,22 @@ const jwt = require("jsonwebtoken");
 const loginController = async (req, res) => {
     try {
         console.log("Logging In");
+        console.log(req.body.email);
         // Find the user with the given email address
         const user = await User.findOne({ email: req.body.email });
+
+        console.log(user);
         // Return a HTTP status of 401 and an error message if no user is found
-        !user && res.status(401).json("Wrong password or username!");
+        if(!user)
+            res.status(401).json("Wrong password or username!");
 
         // Decrypt the user's password and convert it to a string
         const bytes = CryptoJs.AES.decrypt(user.password, process.env.SECRET_KEY);
         const originalPassword = bytes.toString(CryptoJs.enc.Utf8);
 
+        console.log("Password decrypted");
         // Return a HTTP status of 401 and an error message if the password is incorrect
-        originalPassword !== req.body.password &&
+        if (originalPassword !== req.body.password)
             res.send({ "status": "failed", "message": "Email or Password is not Valid" })
 
         // Generate a new JWT with the user's ID and isAdmin status, using the secret key from the environment variables and an expiration of 5 days
@@ -25,8 +30,8 @@ const loginController = async (req, res) => {
             { expiresIn: "5d" }
         );
 
-        // Remove the password from the user object and return it along with the access token
-        // const { password, ...info } = user._doc;
+        console.log("Token Generated");
+
         res.send({ "status": "success", "message": "Login Success", "accessToken": accessToken })
         // res.status(200).json({ ...info, accessToken });
        
